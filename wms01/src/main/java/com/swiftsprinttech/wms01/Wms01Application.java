4b33d4f005f955a6b990691c88e712eaa2f50d60
JiaweiLiu316 <71479153+JiaweiLiu316@users.noreply.github.com>
2024-11-15 14:52:21 +0800

    firstupload
    firstupload

---
+++ wms01/src/main/java/com/swiftsprinttech/wms01/Wms01Application.java
@@ -0,0 +1,13 @@
+package com.swiftsprinttech.wms01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wms01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wms01Application.class, args);
+    }
+
+}
