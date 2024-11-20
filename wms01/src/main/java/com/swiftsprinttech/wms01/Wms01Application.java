a590dc29963e99d6632e385c489f2d44c6d84309
JiaweiLiu316 <jiaweiliu316@163.com>
2024-11-21 02:05:51 +0800

    feat：增加用户接口

--- wms01/src/main/java/com/swiftsprinttech/wms01/Wms01Application.java
+++ wms01/src/main/java/com/swiftsprinttech/wms01/Wms01Application.java
@@ -1,9 +1,14 @@
 package com.swiftsprinttech.wms01;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * @author jiawe
+ */
+@SpringBootApplication // 扫描 Mapper 接口所在包
+@MapperScan("com.swiftsprinttech.wms01.mappers")
 public class Wms01Application {
 
     public static void main(String[] args) {
